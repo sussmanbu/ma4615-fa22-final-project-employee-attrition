1a4d889977ca04a8f09c2b2f68d8cf58ca327752
#Minghao Sun# <#smh0723@bu.edu#>
2022-11-21 03:10:51 -0500

    .

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -15,13 +15,3 @@ fossil_fuel<-read.csv(here::here("dataset", "global_fossil_fuel_consumption.csv"
 fossil_fuel_clean <- fossil_fuel
 write.csv(fossil_fuel_clean, file = here::here("dataset", "global_fossil_fuel_consumption.csv"))
 save(fossil_fuel_clean, file = here::here("dataset/global_fossil_fuel_consumption.RData"))
-
-Forest_area<-read_csv(here::here("dataset", "API_AG.LND.FRST.ZS_DS2_en_csv_v2_4701080.csv"))
-Forest_area_clean <- Forest_area
-write.csv(Forest_area_clean, file = here::here("dataset", "API_AG.LND.FRST.ZS_DS2_en_csv_v2_4701080.csv"))
-save(Forest_area_clean, file = here::here("dataset/(Forest_area.RData"))
-
-World_population<-read_csv(here::here("dataset", "API_SP.POP.TOTL_DS2_en_csv_v2_4701113.csv"))
-World_population_clean <- World_population
-write.csv(World_population_clean, file = here::here("dataset", "API_SP.POP.TOTL_DS2_en_csv_v2_4701113.csv"))
-save(World_population_clean, file = here::here("dataset/World_population.RData"))
