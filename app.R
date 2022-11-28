78175b6eb47d7809156a2ff279adcbe9bfc18e50
Marielle <mbelomme@bu.edu>
2022-11-27 23:43:06 -0500

    Interactive UI.

---
+++ app.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(shiny)
+
+CO2_emissions <- read_csv(here::here("dataset","historical_emissions.csv"))
+
+
+CO2_clean<-CO2_emissions %>% select(Country,34:6)
+CO2_clean$`1990`<-as.double(CO2_clean$`1990`)
+
+#df <- read_csv("dataset/annual_csv.RData")
+df <- CO2_clean %>% pivot_longer(cols = c("1990":"2018"), names_to = "Year",values_to = "CO2emission")
+
+# Client interface
+ui <- fluidPage(
+  # Application title
+  titlePanel("CO2 Emissions"),
+  
+  #  Sidebar panel for input
+  sidebarPanel(
+    # User inputs year to query from
+    selectInput(
+      inputId = "year",
+      label = "From which year:",
+      choices = df$Year %>% unique()
+    ),
+    
+    # User inputs country to query from
+    selectInput(
+      inputId = "country",
+      selected = "World",
+      label = "Which country are you are interested:",
+      choices = df$Country%>% unique(),
+      multiple = TRUE
+    )
+    
+  ),
+  
+  mainPanel(
+    plotOutput(outputId = "scatter"),
+    textOutput(outputId = "test")
+  )
+  
+)
+
+# Defines server logic
+server <- function(input, output) {
+  output$scatter <- renderPlot({
+    # Centers titles
+    theme_update(plot.title = element_text(hjust = 0.5))
+    
+    # Plots a line for each country selected
+    df %>% 
+      filter(Country %in% input$country & Year >= input$year) %>% 
+      ggplot() + 
+      labs(x = "Year", y = "CO2 Emission", color = "Country", title = "Progression of CO2 Levels") +
+      geom_line(mapping = aes(x = Year, y = CO2emission, group = Country, colour = as.factor(Country)))
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
