0e8d7c83b2d0fac0de607c1859d53fa02d45ba86
Marielle <mbelomme@bu.edu>
2022-11-28 22:14:54 -0500

    Adds leaflet map to shiny app and places circle markers on it where their size is proportional to the country's CO2 emission.

--- app.R
+++ app.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(shiny)
+library(leaflet)
+library(sf)
 
+coordinates <- read_csv(here::here("dataset","coordinates.csv"))
 CO2_emissions <- read_csv(here::here("dataset","historical_emissions.csv"))
 
 
@@ -31,15 +34,23 @@ ui <- fluidPage(
       label = "Which country are you are interested:",
       choices = df$Country%>% unique(),
       multiple = TRUE
-    )
+    ),
     
+    # User inputs year that map should query from
+    sliderInput(
+      inputId = 'mapYear', 
+      label = 'Year', 
+      min = 1990, 
+      max = 2018, 
+      value = c(2000)
+    )
   ),
+  
   
   mainPanel(
     plotOutput(outputId = "scatter"),
-    textOutput(outputId = "test")
+    leafletOutput(outputId = "map")
   )
-  
 )
 
 # Defines server logic
@@ -55,6 +66,23 @@ server <- function(input, output) {
       labs(x = "Year", y = "CO2 Emission", color = "Country", title = "Progression of CO2 Levels") +
       geom_line(mapping = aes(x = Year, y = CO2emission, group = Country, colour = as.factor(Country)))
   })
+  
+  map_df <- reactive({
+    df %>%
+      filter(Year == input$mapYear) %>%
+      left_join(y = coordinates, by = c('Country' = 'coverage_city')) %>%
+      filter(!is.na(lng) & !is.na(lat)) %>%
+      st_as_sf(coords = c('lng', 'lat')) %>%
+      st_set_crs(4326)
+  })
+  # Adds circle markers to map based on level of CO2 emission
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = -95.7, lat = 37, zoom = 2) %>%
+      addCircleMarkers(data = map_df(), radius = ~sqrt(CO2emission))
+  })
+  
 }
 
 # Run the application
