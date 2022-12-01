25e79b092ee1e483c11d4afa6e84604995e5cbd9
#Minghao Sun# <#smh0723@bu.edu#>
2022-12-01 14:13:22 -0500

    .

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -27,6 +27,38 @@ forest_area_clean <- forest_area
 write.csv(forest_area_clean, file = here::here("dataset", "API_AG.LND.FRST.ZS_DS2_en_excel_v2_4700430 (1).csv"))
 save(forest_area_clean, file = here::here("dataset/API_AG.LND.FRST.ZS_DS2_en_excel_v2_4700430 (1).RData"))
 
+CO2_clean<-CO2_emissions %>% select(Country,34:6)
+CO2_clean$`1990`<-as.double(CO2_clean$`1990`)
+CO2_clean_1 <- CO2_clean %>% 
+  pivot_longer(cols = c("1990":"2018"),names_to = "Year",values_to = "CO2emission")
+CO2_clean_1<- CO2_clean_1 %>% group_by(Country,Year)
+
+global_temp<-annual_temperature %>% filter(Source=="GCAG") %>% select(Year,Mean)
+global_temp$Year<-as.character(global_temp$Year)
+World_data<-CO2_clean_1 %>% filter(Country=="World") %>% 
+  inner_join(global_temp)
+
+colnames(fossil_fuel)[which(names(fossil_fuel)=="Gas..TWh..direct.energy.")]<- "Gas"
+colnames(fossil_fuel)[which(names(fossil_fuel)=="Oil..TWh..direct.energy.")]<- "Oil"
+colnames(fossil_fuel)[which(names(fossil_fuel)=="Coal..TWh..direct.energy.")]<- "Coal"
+fossil_fuel<-fossil_fuel %>% select(Year:Coal)
+fossil_fuel$Year<-as.character(fossil_fuel$Year)
+World_data<-fossil_fuel %>% inner_join(World_data)
+
+world_pop<-population %>% filter(`Country Name`=="World") %>% 
+  select(`Country Name`,"1960":"2021") %>% 
+  pivot_longer(cols = c("1960":"2021"),names_to = "Year",values_to = "population")
+World_data<-world_pop %>% inner_join(World_data)
+
+world_forest_area<-forest_area %>% filter(`Country Name`=="World") %>% 
+  select(`Country Name`,"1960":"2021") %>% 
+  pivot_longer(cols = c("1960":"2021"),names_to = "Year",values_to = "forest area")
+World_data<-world_forest_area %>% inner_join(World_data)
+
+World_data<-World_data %>% select(- Country)
+colnames(World_data)[9]<-"temperature"
+colnames(World_data)[1]<-"Country_Name"
+colnames(World_data)[3]<-"forest_area"
 
 
 
